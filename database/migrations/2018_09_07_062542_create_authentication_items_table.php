58f0ed32fc1223b0305e1d13d089b2a235d137b4
philippolidan <cxpolidan@gmail.com>
2018-09-07 14:59:39 +0800

    auth

---
+++ database/migrations/2018_09_07_062542_create_authentication_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthenticationItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authentication_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('abs_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authentication_items');
+    }
+}
