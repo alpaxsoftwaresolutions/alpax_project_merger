e58bc82050596d081f6ed5d389bcbe0fd7b9e24b
Mark Dherrick Cuevas <42988318+dherrickcuevas@users.noreply.github.com>
2018-10-02 15:53:20 +0800

    Merge

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -9,26 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+/
 
 Route::get('/', function () {
-    return view('pages.home');
+	return view('pages.home');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/insert-module','PagesController@insert_module');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-<<<<<<< HEAD
 
-Route::get('/auth', 'authController@index')->name('index.authentications'); 
-=======
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 //View authentications
 Route::get('/auth/', 'authController@index')->name('index.authentications'); 
->>>>>>> 924d7f41fc58897cca59a1aebb0d0b437330e305
 //Saving New authentications
 Route::get('/auth/create', 'authController@create')->name('create.authentications'); 
 Route::post('/auth/create', 'authController@store')->name('store.authentications');
@@ -39,21 +36,6 @@ Route::post('/auth/edit/{authId}', 'authController@update')->name('update.authen
 Route::get('/authItems/edit/{authId}', 'authController@editItem')->name('edit.authenticationItems');  
 Route::post('/authItems/edit/{authId}', 'authController@updateItem')->name('update.authenticationItems');
 //Delete authentications
-<<<<<<< HEAD
-Route::get('/auth/delete/{authId}', 'authController@delete')->name('delete.authentications');
-
-Route::get('/settings/general_settings', function(){
-	return view('settings.general_settings');
-});
-
-Route::get('/settings/company_profile', function(){
-	return view('settings.company_profile');
-});
-
-Route::get('/settings/roles', function(){
-	return view('settings.roles');
-});
-=======
 Route::get('/auth/delete/{authId}', 'authController@delete')->name('delete.authentications'); 
 //View Roles
 Route::get('/role/', 'roleController@index')->name('index.role'); 
@@ -65,4 +47,13 @@ Route::get('/role/edit/{roleId}', 'roleController@edit')->name('edit.role');
 Route::post('/role/edit/{roleId}', 'roleController@update')->name('update.role');
 //Delete role
 Route::get('/role/delete/{roleId}', 'roleController@delete')->name('delete.role'); 
->>>>>>> 924d7f41fc58897cca59a1aebb0d0b437330e305
+//View company
+Route::get('/company/', 'companyController@index')->name('index.company'); 
+//Saving company
+Route::get('/company/edit/{companyID}', 'companyController@edit')->name('edit.company');  
+Route::post('/company/edit/{companyID}', 'companyController@update')->name('update.company');
+//Delete company
+Route::get('/company/delete/{companyID}', 'companyController@delete')->name('delete.company'); 
+//Create Database Table
+Route::get('/table/create/', 'tableController@create')->name('create.table'); 
+Route::post('/table/create/', 'tableController@store')->name('store.table'); 
