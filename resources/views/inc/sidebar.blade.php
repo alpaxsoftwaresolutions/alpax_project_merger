5913e43b85a4df804b09ec5649b871f737167632
ianhistorillo <42988272+ianhistorillo@users.noreply.github.com>
2018-10-02 15:40:49 +0800

    try

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -45,36 +45,14 @@
 </nav> --}}
 
 <!-- Sidebar  -->
-{!!html_entity_decode($sidebar)!!}
+
 <nav id="sidebar">
 	<div class="sidebar-header">
 		<h3><img src="{{asset('img/logo/brand.svg')}}" style="height: 50px; width: 100%"></h3>
 		<strong><img src="{{asset('img/logo/brand.svg')}}" style="height: 50px; width: 100%"></strong>
 	</div>
 
-	<ul class="list-unstyled components">
-		<li id="doctor">
-			<a href="admission_dashboard.php">
-				<i class="fas fa-home"></i>Dashboard
-			</a>
-		</li>
-	</ul>
-
-
-	<ul class="list-unstyled">
-		<li>
-			<a href="#" class="text-danger-200">
-				<i class="fas fa-cogs"></i>
-				Settings
-			</a>
-		</li>
-
-		<li>
-			<a href="logout.php" class="text-danger-200">
-				<i class="fas fa-sign-out-alt"></i>
-				Log out
-			</a>
-		</li>
-	</ul>
+	{!!html_entity_decode($sidebar)!!}
+	
 </nav>
 <!-- Sidebar -->
