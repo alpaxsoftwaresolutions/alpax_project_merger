9ba4682b67b5f0e5d8dea8561a051acc7517296c
philippolidan <cxpolidan@gmail.com>
2018-10-02 15:51:54 +0800

    merge

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
-Route::get('/insert-module','PagesController@insert_module');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -24,8 +22,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+<<<<<<< HEAD
+
+Route::get('/auth', 'authController@index')->name('index.authentications'); 
+=======
 //View authentications
 Route::get('/auth/', 'authController@index')->name('index.authentications'); 
+>>>>>>> 924d7f41fc58897cca59a1aebb0d0b437330e305
 //Saving New authentications
 Route::get('/auth/create', 'authController@create')->name('create.authentications'); 
 Route::post('/auth/create', 'authController@store')->name('store.authentications');
@@ -36,6 +39,21 @@ Route::post('/auth/edit/{authId}', 'authController@update')->name('update.authen
 Route::get('/authItems/edit/{authId}', 'authController@editItem')->name('edit.authenticationItems');  
 Route::post('/authItems/edit/{authId}', 'authController@updateItem')->name('update.authenticationItems');
 //Delete authentications
+<<<<<<< HEAD
+Route::get('/auth/delete/{authId}', 'authController@delete')->name('delete.authentications');
+
+Route::get('/settings/general_settings', function(){
+	return view('settings.general_settings');
+});
+
+Route::get('/settings/company_profile', function(){
+	return view('settings.company_profile');
+});
+
+Route::get('/settings/roles', function(){
+	return view('settings.roles');
+});
+=======
 Route::get('/auth/delete/{authId}', 'authController@delete')->name('delete.authentications'); 
 //View Roles
 Route::get('/role/', 'roleController@index')->name('index.role'); 
@@ -47,3 +65,4 @@ Route::get('/role/edit/{roleId}', 'roleController@edit')->name('edit.role');
 Route::post('/role/edit/{roleId}', 'roleController@update')->name('update.role');
 //Delete role
 Route::get('/role/delete/{roleId}', 'roleController@delete')->name('delete.role'); 
+>>>>>>> 924d7f41fc58897cca59a1aebb0d0b437330e305
